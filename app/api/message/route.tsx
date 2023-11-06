96559273ec661f6a359e2f570f4ac194693875c1
Okysu <yby@ecanse.com>
2023-11-06 21:10:46 +0800

    add home page random message

--- app/api/message/route.tsx
+++ app/api/message/route.tsx
@@ -27,32 +27,42 @@ interface Comment {
 }
 
 export async function GET() {
-  const count = await fetch(
-    "https://waline.zcy.zone/api/comment?type=count&path=record"
-  );
-  const data = (await count.json()) as Response;
+  try {
+    const count = await fetch(
+      "https://waline.zcy.zone/api/comment?type=count&path=record"
+    );
+    const data = (await count.json()) as Response;
 
-  const total = data.data as number;
-  const page = Math.ceil(total / 10);
-  let random = Math.floor(Math.random() * page) + 1;
+    const total = data.data as number;
+    const page = Math.ceil(total / 10);
+    let random = Math.floor(Math.random() * page) + 1;
 
-  const message = await fetch(
-    "https://waline.zcy.zone/api/comment?path=record&page=" + random
-  );
+    const message = await fetch(
+      "https://waline.zcy.zone/api/comment?path=record&page=" + random
+    );
 
-  const messageData = (await message.json()) as Response;
-  const messageList = messageData.data.data as Comment[];
-  const randomMessage = Math.floor(Math.random() * 10) + 1;
-  const content = messageList[randomMessage];
+    const messageData = (await message.json()) as Response;
+    const messageList = messageData.data.data as Comment[];
+    const randomMessage = Math.floor(Math.random() * 10) + 1;
+    const content = messageList[randomMessage];
 
-  content.orig = content.comment.replace(/<[^>]+>/g, "");
-  content.orig = content.orig.replace(/[\r\n]/g, "");
+    content.orig = content.comment.replace(/<[^>]+>/g, "");
+    content.orig = content.orig.replace(/[\r\n]/g, "");
 
-  const response = {
-    code: 200,
-    message: "success",
-    data: content.orig,
-  };
+    const response = {
+      code: 200,
+      message: "success",
+      data: content.orig,
+    };
 
-  return NextResponse.json(response);
+    return NextResponse.json(response);
+  } catch (error) {
+    console.error(error);
+    const response = {
+      code: 500,
+      message: "error",
+      data: null,
+    };
+    return NextResponse.json(response);
+  }
 }
